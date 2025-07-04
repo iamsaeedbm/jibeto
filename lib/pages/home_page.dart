b863146b6044e115c4f89fd88b7c05d2cc3e0d25
saeedbm <iamsaeedbm@gmail.com>
2025-07-04 22:07:13 +0330

    update & modified  files

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:jibeto_app/theme/nav_bar.dart';
-import 'package:jibeto_app/theme/fab_button.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,17 +10,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      // fab button
-      floatingActionButton: getFab(),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      // bottom nav bar
-      bottomNavigationBar: getBottomNavBar(),
-      // main body
-      body: const SafeArea(
-        child: Center(),
-      ),
-    );
+    return const Placeholder();
   }
 }
